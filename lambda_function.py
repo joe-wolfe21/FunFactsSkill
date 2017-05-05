66e14ccc3e5df3578f29c3f0a25e740f4eff205b
Wolfe <joseph.wolfe@capitalone.com>
2017-05-05 12:04:09 -0400

    updates

---
+++ lambda_function.py
@@ -0,0 +1,38 @@
+from ask import alexa
+from random import randint
+
+def lambda_handler(request_obj, context={}):
+    return alexa.route_request(request_obj)
+
+@alexa.default_handler()
+def default_handler(request):
+    return launch_request_handler(request)
+
+@alexa.request_handler("LaunchRequest")
+def launch_request_handler(request):
+    return alexa.create_response(message="Welcome to Fun Fact!",
+                                 reprompt_message='Would you like a fun fact?')
+
+@alexa.request_handler(request_type="SessionEndedRequest")
+def session_ended_request_handler(request):
+    return alexa.create_response(message="Bye!", end_session=True)
+
+@alexa.intent_handler("GetFactIntent")
+def get_fact_intent_handler(request):
+    linesOfFacts = [line.rstrip('\n') for line in open('facts.txt')]
+    randFact = (randint(0,len(linesOfFacts)-1))
+    fact = linesOfFacts[randFact]
+
+    return alexa.create_response(message=fact, end_session=True)
+
+@alexa.intent_handler("AMAZON.HelpIntent")
+def help_intent_handler(request):
+    return alexa.create_response(message="This skill tells you a fun fact!", end_session=False)
+
+@alexa.intent_handler("AMAZON.StopIntent")
+def stop_intent_handler(request):
+    return alexa.create_response(message="Bye!", end_session=True)
+
+@alexa.intent_handler("AMAZON.CancelIntent")
+def cancel_intent_handler(request):
+    return alexa.create_response(message="Bye!", end_session=True)
